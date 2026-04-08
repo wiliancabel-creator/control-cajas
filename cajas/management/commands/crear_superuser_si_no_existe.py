ec49e5bc83c80fc948bd08a41936a767e23b3c4e
wiliancabel-creator <wiliancabel@gmail.com>
2026-04-08 15:49:27 -0600

    Crear superusuario automatico en deploy

---
+++ cajas/management/commands/crear_superuser_si_no_existe.py
@@ -0,0 +1,36 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+import os
+
+
+class Command(BaseCommand):
+    help = "Crea un superusuario si no existe"
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+
+        username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+        email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+        if not username or not email or not password:
+            self.stdout.write(self.style.WARNING(
+                "Faltan variables de entorno para crear el superusuario."
+            ))
+            return
+
+        if User.objects.filter(username=username).exists():
+            self.stdout.write(self.style.SUCCESS(
+                f"El superusuario '{username}' ya existe."
+            ))
+            return
+
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password
+        )
+
+        self.stdout.write(self.style.SUCCESS(
+            f"Superusuario '{username}' creado correctamente."
+        ))
